9a8c7711fae18fa5e2b91f6f01d4fb87142c80dc
Luis Perez Smith <lperezs@indra.es>
2026-04-20 16:51:08 +0200

    Add Print test library

---
+++ cmake/projects/Print/hunter.cmake
@@ -0,0 +1,21 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+include(hunter_cmake_args)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Print
+    VERSION
+    "v0.0.1"
+    URL
+    "https://github.com/luispsm/hunter_test-dep/archive/refs/tags/v0.0.1.tar.gz"
+    SHA1
+    61f505b0518e33e1ff4bb0960aae9a773dc35bc8
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(Print)
+hunter_download(PACKAGE_NAME Print)
+
